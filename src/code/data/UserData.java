ef1eb09c3d18f82413e17c603fd99083a081aa0d
qorvhfl080 <86284854+qorvhfl080@users.noreply.github.com>
2021-09-02 13:05:58 +0900

    사용자 클래스 추가

---
+++ src/code/data/UserData.java
@@ -0,0 +1,41 @@
+package code.data;
+
+public class UserData {
+	
+	private String name;
+	private String phoneNum;
+	private int birthYear;
+	
+	public UserData(String name, String phoneNum, int birthYear) {
+		this.name = name;
+		this.phoneNum = phoneNum;
+		this.birthYear = birthYear;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPhoneNum() {
+		return phoneNum;
+	}
+
+	public void setPhoneNum(String phoneNum) {
+		this.phoneNum = phoneNum;
+	}
+
+	public int getBirthYear() {
+		return birthYear;
+	}
+
+	public void setBirthYear(int birthYear) {
+		this.birthYear = birthYear;
+	}
+	
+		
+
+}
